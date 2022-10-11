23b59f948a8866b90bb551df510e0477c43dd5a3
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-10-11 16:47:22 +0100

    4
    finish Head or Tail game

--- app/src/main/java/com/cfjahnprojects/sorteios/HeadTail.java
+++ app/src/main/java/com/cfjahnprojects/sorteios/HeadTail.java
@@ -3,8 +3,13 @@ package com.cfjahnprojects.sorteios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class HeadTail extends AppCompatActivity {
 
@@ -16,5 +21,44 @@ public class HeadTail extends AppCompatActivity {
     public void mainScreen(View view){
         Intent screen = new Intent(this,MainActivity.class);
         startActivity(screen);
+    }
+    public void playerHead(View view){
+        maincallHT("Head");
+        TextView txPlayerChoice = findViewById(R.id.playerChoice);
+        txPlayerChoice.setText("You choose Head!!!");
+    }
+    public void playerTail(View view){
+        maincallHT("Tail");
+        TextView txPlayerChoice = findViewById(R.id.playerChoice);
+        txPlayerChoice.setText("You choose Tail!!!");
+    }
+    private void maincallHT(String s){
+        int num = new Random().nextInt(2);
+        setPicFinalCoin(num);
+        whoWon(s, num);
+
+    }
+    private void setPicFinalCoin(int num){
+        ImageView imaVi = findViewById(R.id.resultCircleCoin);
+        switch (num){
+            case 0:
+                imaVi.setImageResource(R.drawable.head);
+                break;
+            case 1:
+                imaVi.setImageResource(R.drawable.tail);
+                break;
+        }
+    }
+    private void whoWon(String s, int num){
+        TextView TxVi = findViewById(R.id.resultHT);
+        if((s == "Head" && num ==  0) || (s == "Tail" && num ==  1)){
+            TxVi.setText("You Won!!!");
+            TxVi.setTextColor(Color.BLUE);
+        }else{
+            TxVi.setText("You Lost!!!");
+
+            TxVi.setTextColor(Color.RED);
+        }
+
     }
 }
